795854301ad7ab51b24ef52ddeb356ae587c5d7d
he03villa <he03villa@gmail.com>
2018-10-22 14:06:54 -0500

    agregando funciones

--- resources/views/proyec/show.blade.php
+++ resources/views/proyec/show.blade.php
@@ -206,6 +206,12 @@
     <div class="modal fade" id="logout" role="dialog">
         <div class="modal-dialog">
             <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Iniciar sesión</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
                 <div class="model-body">
                     <label>Para poder donar tienen que a ver iniciado sesión</label>
                 </div>
